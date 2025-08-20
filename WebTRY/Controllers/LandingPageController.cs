31eeb0d4e64958836d710a8699e9064004c6ea3a
Joshua-24-c <joshua.pagkaliwangan.2403@gmail.com>
2025-08-20 16:16:03 +0800

    Add project files.

---
+++ WebTRY/Controllers/LandingPageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTRY.Models;   
+
+namespace WebTRY.Controllers
+{
+    public class LandingPageController : Controller
+    {
+        // Landing Page
+        public ActionResult Index()
+        {
+            ViewBag.HeadT = "Welcome to Bastion Incorporation!";
+            ViewBag.SubT = "We’re thrilled to have you join our team as an intern! To get started and complete your onboarding, please register by clicking the button below. If you’ve already registered, simply scan your QR code to continue.";
+            return View();
+        }
+
+        // Show Registration Form (GET)
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // Handle Registration Form (POST)
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+
+        public ActionResult QR()
+        {
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebTRY/Models/RegisterModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebTRY.Models
+{
+    public class RegisterModel
+    {
+        [Required (ErrorMessage = " First name is required.")]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$", ErrorMessage = " Invalid first name.")]
+        public string FirstName { get; set; }
+
+        [Required (ErrorMessage = " Last name is required.")]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$", ErrorMessage = " Invalid last name.")]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress(ErrorMessage = " Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required, Phone(ErrorMessage = " Invalid phone number.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = " Gender is required.")]
+        public string Gender { get; set; }
+
+        [Range(1, 120,ErrorMessage = " Invalid age.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = " University name is required.")]
+        public string UnivName { get; set; }
+
+        [Required(ErrorMessage = " Course is required.")]
+        public string Course { get; set; }
+
+        [Required(ErrorMessage = " Year level is required.")]
+        public string YearLevel { get; set; }
+
+        [Required(ErrorMessage = " Job role is required.")]
+        public string Role { get; set; }
+
+        [Required(ErrorMessage = " Assigned Department is required.")]
+        public string Department { get; set; }
+
+        [Required(ErrorMessage = " Required time is required.")]
+        public int Time { get; set; }
+
+
+    }
+}
